44869be607b6884fc31910d276f7815f28d2841d
Naruhito Kubota <n.kubota@opt.ne.jp>
2019-06-16 22:41:46 +0900

    :sushi: Ready to first release

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
-/*
- * Sample plugin scaffolding for Adobe XD.
- *
- * Visit http://adobexdplatform.com/ for API docs and more sample code.
- */
+const { Text, Rectangle, Polygon, Ellipse, Path } = require("scenegraph");
+const { error } = require("./lib/dialogs.js");
 
+function displayColorText(selection) {
+  const tnum = selection.items.length;
+  for(let i = 0; i < tnum; i++ ) {
 
-const {Rectangle, Color} = require("scenegraph"); 
+    const t = selection.items[i];
 
-function rectangleHandlerFunction(selection) { 
+    const tType = t instanceof Rectangle || t instanceof Polygon || t instanceof Ellipse || t instanceof Path;
+    if (!tType) {
+      error("Sorry,", "this plugin supports only Rectangle, Polygon, Ellipse and Path.");
+      return;
+    }
 
-    const newElement = new Rectangle(); 
-    newElement.width = 100;
-    newElement.height = 50;
-    newElement.fill = new Color("Purple");
+    // 選択したオブジェクトの色の取得
+    const colorValue = t.fill.value;
 
-    selection.insertionParent.addChild(newElement);
-    newElement.moveInParentCoordinates(100, 100);
+      // Color Valueが返ってこないのはグラデーションかイメージフィル
+      if(!colorValue) {
+        error("Sorry,", "this plugin supports only mono fill. You can't select gradient, image fill.");
+        return;
+      }
 
+    const hexZeroPadding = colorValue.toString(16).padStart(8, '0');
+    const hexOpacity = hexZeroPadding.slice(0, 2);
+    const hexColor = hexZeroPadding.slice(2, 8);
+
+    const boundsTargetX = t.boundsInParent.x;
+    const boundsTargetY = t.boundsInParent.y;
+
+    let hexText = new Text();
+    hexText.text = `#${hexColor}`;
+    hexText.fill = t.fill;
+    hexText.fontSize = 36;
+    selection.insertionParent.addChild(hexText);
+    hexText.moveInParentCoordinates(boundsTargetX, boundsTargetY);
+  }
 }
 
 module.exports = {
-    commands: {
-        createRectangle: rectangleHandlerFunction
-    }
+  commands: {
+    displayColors: displayColorText
+  }
 };
